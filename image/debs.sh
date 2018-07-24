f3d1804944cb2263920745697e2648ac832f6277
Will Maier <wcmaier@gmail.com>
2018-07-24 03:17:57 +0000

    Add bazel deps

--- image/debs.sh
+++ image/debs.sh
@@ -6,5 +6,11 @@ docker run --rm -ti ubuntu:18.04 /bin/bash -c \
     "apt-get update \
     && apt-get install --print-uris \
     git \
+    pkg-config \
+    zip \
+    g++ \
+    zlib1g-dev \
+    unzip \
+    python \
     " \
     | python image/debs.py > image/image.bzl
